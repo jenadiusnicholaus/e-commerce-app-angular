121c7a3457c02031d9d844c09f94b4657e9f6cff
jenadiusnicholaus <ucode888@gmail.com>
2023-01-14 14:09:20 +0300

    added , a add to cart, checkout feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AppTopBarComponent } from './app-top-bar/app-top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDardComponent } from './product-dard/product-dard.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
 
 @NgModule({
   declarations: [
@@ -14,11 +21,16 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     AppTopBarComponent,
     ProductListComponent,
     ProductDardComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
